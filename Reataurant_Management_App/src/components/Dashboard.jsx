9a07fa7c9b9727bf17873567a8ab537c26bb21d7
Mahalaxmi-Komuravelly <mahalaxmikomuravelly@gmail.com>
2026-01-03 20:50:21 +0530

    login page

--- Reataurant_Management_App/src/components/Dashboard.jsx
+++ Reataurant_Management_App/src/components/Dashboard.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { RestaurantContext } from '../context/RestaurantContext'
 
 const Dashboard = () => {
-    const {res}
+    const {restuarants} = useContext(RestaurantContext);
+
   return (
     <div>
-      
+      {restuarants.length === 0? (<p>
+        no data
+      </p>):
+      (
+        <div>
+            {
+              restuarants.map((res,index)=>(
+                
+                <div>
+                    <img src="{res.image}"/>
+                    <h3>{res.restaurantName}</h3>
+                    <p>{res.type}</p>
+                    <p>{res.address}</p>
+                 </div>   
+              ))  
+            }
+        </div>
+      )}
     </div>
   )
 }
