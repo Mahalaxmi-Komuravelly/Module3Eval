39a822c392c7d5e1a4af5457054c640181c4946f
Mahalaxmi-Komuravelly <mahalaxmikomuravelly@gmail.com>
2026-01-03 20:53:16 +0530

    login page

--- Reataurant_Management_App/src/pages/CustomerDashboard.jsx
+++ Reataurant_Management_App/src/pages/CustomerDashboard.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Dashboard from '../components/Dashboard'
 
 const CustomerDashboard = () => {
   return (
     <div>
-      
+      <Dashboard/>
     </div>
   )
 }
